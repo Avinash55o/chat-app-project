2af3f3afce35c756a83156981239adb5a77d2bd4
Avinash Boruah <avinashboruah8@gmail.com>
2025-05-12 19:05:20 +0530

    creating the user model

--- models/user.models.js
+++ models/user.models.js
@@ -1,5 +1,35 @@
 import mongoose from "mongoose";
 
-const userSchema= new mongoose.Schema({},{timestamps:true});
+
+const userSchema= new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        index: true,
+        unique:true
+    },
+    fullName:{
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    email:{
+        type: String,
+        required: true,
+        lowercase: true,
+        unique:true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength:[8, "password must be 8 characters"]
+    },
+    avatar:{
+        type: String
+    },
+    refreshToken:{
+        type: String,
+    }
+},{timestamps:true});
 
 export const User= mongoose.model("User",userSchema)

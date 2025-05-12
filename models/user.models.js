03863f674eb770f73ffeaba2f4d850afcebe5c15
Avinash Boruah <avinashboruah8@gmail.com>
2025-05-12 19:09:21 +0530

    added prettier

--- models/user.models.js
+++ models/user.models.js
@@ -1,35 +1,37 @@
 import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      index: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [8, "password must be 8 characters"],
+    },
+    avatar: {
+      type: String,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-const userSchema= new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-        index: true,
-        unique:true
-    },
-    fullName:{
-        type: String,
-        required: true,
-        lowercase: true
-    },
-    email:{
-        type: String,
-        required: true,
-        lowercase: true,
-        unique:true
-    },
-    password:{
-        type: String,
-        required: true,
-        minlength:[8, "password must be 8 characters"]
-    },
-    avatar:{
-        type: String
-    },
-    refreshToken:{
-        type: String,
-    }
-},{timestamps:true});
-
-export const User= mongoose.model("User",userSchema)
+export const User = mongoose.model("User", userSchema);

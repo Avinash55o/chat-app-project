4450e76b8ffa03f6100d172f79d1ea16527c2cda
Avinash Boruah <avinashboruah8@gmail.com>
2025-05-13 00:50:43 +0530

    model and controller

--- models/chat.models.js
+++ models/chat.models.js
@@ -1,5 +1,31 @@
 import mongoose from "mongoose";
 
-const chatSchema = new mongoose.Schema({}, { timestamps: true });
+const chatSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+    lastMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    participents: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 export const Chat = mongoose.model("Chat", chatSchema);
--- models/message.models.js
+++ models/message.models.js
@@ -1,5 +1,29 @@
 import mongoose from "mongoose";
 
-const userMessageSchema = new mongoose.Schema({}, { timestamps: true });
+const userMessageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+    },
+    attachments: {
+      type: [
+        {
+          url: String,
+          localpath: String,
+        },
+      ],
+      default: [],
+    },
+    chat: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "chat",
+    },
+  },
+  { timestamps: true }
+);
 
 export const Message = mongoose.model("Message", userMessageSchema);

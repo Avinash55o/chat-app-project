e001d6fb2c1010f0e7ca75c19933aa185aad50ac
Avinash Boruah <avinashboruah8@gmail.com>
2025-05-15 18:20:56 +0530

    add the utlis cloudinary to upload images

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,1 +1,33 @@
-impo
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+
+//configuration
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+//Logic to upload an image
+const uploadToCloudinay= async (localFilePath)=>{
+  try {
+    //Console.log("file is here to upload to cloudinary",localFilePath);
+
+    if(!localFilePath) return null;
+   
+    // uploading the image
+    const response= await cloudinary.uploader.upload(localFilePath,{
+      resource_type: "auto"
+    });
+
+    return response
+
+  } catch (error) {
+    // Delete the file from the server(temp)
+    fs.unlinkSync(localFilePath)
+    return null;
+  }
+}
+
+export {uploadToCloudinay};
